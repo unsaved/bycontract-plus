9b0a2090d032d513748081c2191833581bbcc533
Blaine Simpson <blaine.simpson@admc.com>
2022-06-30 09:51:01 -0400

    Optimized ESLint then satisfied it

--- bycontractOverride.js
+++ bycontractOverride.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { format } = require("util");
 const byContract = require("bycontract");
 module.exports.Exception = byContract.Exception;
@@ -5,18 +7,18 @@ module.exports.is = byContract.is;
 module.exports.validate = blaineValidate;  // eslint-disable-line no-use-before-define
 
 byContract.is.int = val => Number.isInteger(val);
-byContract.is.date = val => val !== null && typeof(val) === "object" && (val instanceof Date);
+byContract.is.date = val => val !== null && typeof val === "object" && val instanceof Date;
 byContract.is.posint = val => Number.isInteger(val) && val > 0;
 byContract.is.nonnegint = val => Number.isInteger(val) && val >= 0;
 byContract.is.posnum = val => Number.isNumber(val) && val > 0;
 byContract.is.isotimestr = val =>  // ISO 8601
-    typeof(val) === "string" &&
+    typeof val === "string" &&
     /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(?:[.]\d+)?(?:[+-]\d\d|[+-]\d\d?:\d\d)?Z?$/.test(val);
 // This is ISO 8601 format to second resolution with no optional additions.
 byContract.is.isotimestr_s = val =>  // eslint-disable-line camelcase
-    typeof(val) === "string" && /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/.test(val);
+    typeof val === "string" && /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/.test(val);
 byContract.is.plainobject = val =>
-    typeof(val) === "object" && val !== null &&
+    typeof val === "object" && val !== null &&
       Object.getPrototypeOf(val) === Object.getPrototypeOf({});
 class DoccedValError extends byContract.Exception {
     constructor() {
@@ -38,29 +40,30 @@ module.exports.DoccedValError = DoccedValError;
  */
 function blaineValidate() {
     const argsArray = Array.prototype.slice.call(arguments);
-    if (argsArray.length < 2)
+    if (argsArray.length < 2)  // eslint-disable-next-line prefer-template
         throw new Error(".validate called with " + argsArray.length
           + " params, but requires at least a candidate value + contract");
     let doEnforceArgsLength = true, failMsg;
-    if (argsArray.length > 2 && typeof(argsArray[2]) === "boolean") {
+    if (argsArray.length > 2 && typeof argsArray[2] === "boolean") {
         doEnforceArgsLength = argsArray[2];
         argsArray.splice(2, 1);
     }
     if (argsArray.length > 2)
         failMsg = format.apply(null, argsArray.splice(2, argsArray.length - 2));
     if (doEnforceArgsLength && arguments[1] instanceof Array
-      && ("length" in arguments[0])
-      && arguments[0].length > arguments[1].length)
+      && "length" in arguments[0] && arguments[0].length > arguments[1].length)
         throw new DoccedValError("ETOOMANYARGS",
-          (failMsg === undefined ? "" : (failMsg + "\n"))
+          /* eslint-disable prefer-template */
+          (failMsg === undefined ? "" : `${failMsg}\n`)
           + "Called with " + arguments[0].length + " arguments but at most "
           + arguments[1].length + " are supported");
+          /* eslint-enable prefer-template */
     if (failMsg === undefined)
         return byContract.validate.apply(null, argsArray);
     try {
         return byContract.validate.apply(null, argsArray);
     } catch (e0) {
-        if (typeof(e0) !== "object" || !("name" in e0) || e0.name !== "ByContractError") throw e0;
-        throw new DoccedValError(e0.code, failMsg + "\n" + e0.message);
+        if (typeof e0 !== "object" || !("name" in e0) || e0.name !== "ByContractError") throw e0;
+        throw new DoccedValError(e0.code, `${failMsg}\n${e0.message}`);
     }
 }
